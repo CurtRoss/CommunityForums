30268de74a4a2996527879bb9461409a8be13826
Charles Lucas <charlesdlucas1975@gmail.com>
2021-02-04 21:35:57 -0500

    Charles1

---
+++ CommunityForums.Models/CommentCreate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunityForums.Models
+{
+    public class CommentCreate
+    {
+        [Required, MinLength(2, ErrorMessage = "Please enter at least two characters.")]
+        [MaxLength(500, ErrorMessage = "There are too many characters in this field.")]
+        public string Text { get; set; }
+    }
+}
--- CommunityForums.Models/CommentEdit.cs
+++ CommunityForums.Models/CommentEdit.cs
@@ -1,21 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CommunityForums.Data
+namespace CommunityForums.Models
 {
-    public class Comment
+    public class CommentEdit
     {
-        [Key]
         public int Id { get; set; }
-
-        [Required]
         public string Text { get; set; }
-
-        [Required]
-        public Guid Author { get; set; }
     }
 }
